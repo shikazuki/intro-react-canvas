f6a50f94491576e584a519052c0104993749a1ba
shikazuki <shikazuki0@gmail.com>
2021-03-13 16:57:26 +0900

    add moving box.

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,75 @@ import './App.css';
 import { useCanvas, canvasHelper } from './hooks/canvasHooks';
 import { Box } from './shape';
 
-const COLORS = ['red', 'blue', 'green'];
-
 function App() {
   const { context, canvasEl } = useCanvas();
   const [boxes, setBoxes] = useState<Array<Box>>([]);
-  const [color, setColor] = useState('red');
+  const [activeId, setActiveId] = useState('');
+  const [dragged, setDragged] = useState(false);
   useEffect(() => {
     if (!context) return;
     const helper = canvasHelper({ context, canvasEl });
 
     helper.clearAll();
 
-    context.fillStyle = color;
-    context.fillRect(0, 0, helper.getCanvasWidth(), helper.getCanvasHeight());
-    boxes.forEach(b => b.draw(context));
-  }, [color, context, canvasEl, boxes]);
+    boxes.forEach(b => b.draw(context, activeId));
+  }, [context, canvasEl, boxes, activeId]);
   return (
     <div className="App">
       <h1>Canvas Drawing App</h1>
-      <canvas ref={canvasEl} onClick={(e: React.MouseEvent<HTMLCanvasElement>) => {
-        const rect = e.currentTarget.getBoundingClientRect();
-        if (!rect) return;
-        const x = e.clientX - rect.x;
-        const y = e.clientY - rect.y;
-        setBoxes([...boxes, new Box(x, y, 30, 30, 'blue')]);
-      }} />
+      <canvas ref={canvasEl}
+              width={1200}
+              height={500}
+              onClick={(e: React.MouseEvent<HTMLCanvasElement>) => {
+                const rect = e.currentTarget.getBoundingClientRect();
+                if (!rect) return;
+                const touchedBox = boxes
+                  .filter(b => b.isTouchedIn(e))
+                  .reduce((touched: Box | null, box: Box) => {
+                    if (touched === null) return box;
+                    if (touched.index <= box.index) return box;
+                    return touched;
+                  }, null);
+                if (touchedBox) {
+                  setActiveId(touchedBox.id);
+                  return;
+                }
+              }}
+              onMouseDown={(e: React.MouseEvent<HTMLCanvasElement>) => {
+                const rect = e.currentTarget.getBoundingClientRect();
+                if (!rect) return;
+                const touchedBox = boxes
+                  .filter(b => b.isTouchedIn(e))
+                  .reduce((touched: Box | null, box: Box) => {
+                    if (touched === null) return box;
+                    if (touched.index <= box.index) return box;
+                    return touched;
+                  }, null);
+                if (touchedBox) {
+                  setActiveId(touchedBox.id);
+                  setDragged(true);
+                  return;
+                }
+              }}
+              onMouseMove={(e) => {
+                if (!dragged) return;
+                const newBoxes = boxes.map(b => {
+                  if (b.id !== activeId) return b;
+                  b.x = e.clientX - e.currentTarget.offsetLeft - (b.width / 2);
+                  b.y = e.clientY - e.currentTarget.offsetTop - (b.height / 2);
+                  return b;
+                });
+                setBoxes(newBoxes);
+              }}
+              onMouseUp={() => {
+                setDragged(false);
+              }}
+      />
       <div className="buttons">
-        {COLORS.map(c => (<button key={c} onClick={() => setColor(c)}>{c}</button>))}
+        <button className="button" onClick={() => {
+          const maxIndex = Math.max(...boxes.map(b => b.index)) + 1;
+          setBoxes([...boxes, new Box(10, 10, 30, 30, maxIndex)]);
+        }}>Add Box</button>
       </div>
     </div>
   );
--- src/shape.ts
+++ src/shape.ts
@@ -1,21 +1,47 @@
+import React from "react";
+
+let id = 0;
+const getUniqueId = (prefix: string = '') => {
+  return `${prefix}${id++}`;
+};
 
 export class Box {
-  private x: number;
-  private y: number;
-  private width: number;
-  private height: number;
-  private color: string;
+  public id: string;
+  public x: number;
+  public y: number;
+  public width: number;
+  public height: number;
+  public index: number;
+  public color: string;
 
-  constructor(x: number, y: number, width: number, height: number, color: string = 'black') {
+  constructor(x: number, y: number, width: number, height: number, index: number, color: string = 'black') {
+    this.id = getUniqueId('box');
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.index = index;
     this.color = color;
   }
 
-  draw(context: CanvasRenderingContext2D) {
+  draw(context: CanvasRenderingContext2D, activeId: string) {
     context.fillStyle = this.color;
     context.fillRect(this.x, this.y, this.width, this.height);
+    if (this.id === activeId) {
+      context.lineWidth = 3;
+      context.strokeStyle = 'green';
+      context.strokeRect(this.x, this.y, this.width, this.height);
+    }
+  }
+
+  isTouchedIn(event: React.MouseEvent<HTMLCanvasElement>) {
+    const rect = event.currentTarget.getBoundingClientRect();
+    if (!rect) return;
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    return this.x <= x
+      && x <= (this.x + this.width)
+      && this.y <= y
+      && y <= (this.y + this.height);
   }
 }
